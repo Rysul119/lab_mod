1cc399e66815f629eb1195e09d7c910a8ade5441
Md Rysul Kabir <52078143+Rysul119@users.noreply.github.com>
2021-11-24 17:35:29 -0500

    Update ready_set_go_factory.lua

--- game_scripts/levels/contributed/psychlab/factories/ready_set_go_factory.lua
+++ game_scripts/levels/contributed/psychlab/factories/ready_set_go_factory.lua
@@ -330,10 +330,11 @@ function factory.createLevelApi(kwargs)
     print('timeout')
     print('Timeout happened at interval '..tostring(self.currentTrial.interval))
     for playerId, inv in pairs(custom_observations.playerInventory) do
-      local v, h, _ = unpack(inv:eyeAngles())
+      -- checks player's eye position in world units
+      local xcor, ycor, zcor = unpack(inv:eyePos())
       self:logEyes(v, h)
       -- print('eyes at ', string.format('block_%d_episode_%d_trial_%d_%f-%f', self.blockId, self.episodeId, self.trialId, v, h))
-      print('At timeout eyes at '.. tostring(v).. ','..tostring(h))
+      print('At timeout eyes at '.. tostring(xcor).. ','..tostring(ycor).. ','..tostring(zcor))
     end
     self:finishTrial(kwargs.intertrialInterval)
   end
